b1bea6d1497653200ca1fdba47ca1e0ee2bed0cf
rdevpooja <rdevpooja91@gmail.com>
2021-12-10 21:04:24 +0530

    first codenew2 commit

---
+++ application/views/site.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- <link rel="image/icon" href=""> -->
+        <title>Ratti Vimal</title>
+    </head>
+<body>
+    <div class="p_container">
+        Hello
+    </div>
+</body>
+</html>
+
